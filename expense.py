fbe7ac770fe40974fd8b2590aff02abcb15665cd
bastien <bastien.pamphile@epita.fr>
2023-09-20 21:54:21 +0200

    exercice presque fini

--- expense.py
+++ expense.py
@@ -1,4 +1,13 @@
 from PyInquirer import prompt
+import csv
+
+def load_users():
+    users = []
+    with open("users.csv", 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            users.append(row[0])  # Assuming the name is in the first column
+    return users
 
 expense_questions = [
     {
@@ -12,19 +21,45 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": load_users()
     },
+    {
+        "type":"list",
+        "name":"involved_people",
+        "message":"New Expense - Involved People : ",
+        "choices": load_users()
+    }
+]
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
 
-
 def new_expense(*args):
-    infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
+    # Prompt the user for expense information
+    expense_data = prompt(expense_questions)
+    # Save the expense data to the CSV file
+    with open("expense_report.csv", 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([expense_data['amount'], expense_data['label'], expense_data['spender'], expense_data['involved_people']])
+    print("Expense Added!")
     return True
 
+def new_user(*args):
+    # Prompt the user for expense information
+    user_data = prompt(user_questions)
+    # Save the expense data to the CSV file
+    with open("users.csv", 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([user_data['name']])
+    print("User Added!")
+    return True
 
